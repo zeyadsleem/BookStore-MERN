2e44a575e7070907269093210dde9b799cac7e3b
zeyadsleem <zeyadsleem1@gmail.com>
2024-02-22 22:37:00 +0200

    add create books endpoint

--- server/index.js
+++ server/index.js
@@ -12,6 +12,29 @@ app.get("/", (req, res) => {
   return res.status(234).send("here we are");
 });
 
+// Route for Save a new Book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.auther || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, auther, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      auther: req.body.auther,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
 mongoose
   .connect(mongoDB_URL)
   .then(() => {
