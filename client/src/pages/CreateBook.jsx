a247db514544b0fa7bceaf8808cd1b3a02f624bb
zeyadsleem <zeyadsleem1@gmail.com>
2024-02-24 17:14:59 +0200

    finish book store app

--- client/src/pages/CreateBook.jsx
+++
@@ -1,9 +0,0 @@
-const CreateBook = () => {
-  return (
-    <div>
-      <h1>CreateBook</h1>
-    </div>
-  );
-};
-
-export default CreateBook;
--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-
 import { PORT, mongoDB_URL } from "./config.js";
+import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
-app.use("/books", booksRoute);
 
 // Middleware for handling CORS POLICY
 app.use(cors());
+
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDB_URL)
   .then(() => {
     console.log("App connected to database");
-
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((error) => {
+    console.log(error);
   });
